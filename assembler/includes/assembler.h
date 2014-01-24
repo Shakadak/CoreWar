d116320ced0a298a60a51cbf71dc607056a2ce15
npineau <nofigh@hotmail.fr>
2014-01-24 14:07:52 +0100

    ajout main et header in assembler

---
+++ assembler/includes/assembler.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   assembler.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/24 13:59:58 by npineau           #+#    #+#             */
+/*   Updated: 2014/01/24 14:06:58 by npineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASSEMBLER_H
+# define ASSEMBLER_H
+
+typedef struct		s_clst
+{
+	char			*token;
+	int				type;
+	struct s_clst	next;
+}					t_clst;
+
+void	translate(char *file);
+void	gather(t_clst *tokens);
+int		tokenize(
+
+#endif
