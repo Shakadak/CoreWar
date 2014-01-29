85163366bf87ce374e9816e7fabd41525677ca8a
npineau <nofigh@hotmail.fr>
2014-01-29 17:03:40 +0100

    updt

--- virtual_machine/init_reg.c
+++ virtual_machine/init_reg.c
@@ -1,32 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_reg.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/01/29 14:53:50 by npineau           #+#    #+#             */
-/*   Updated: 2014/01/29 17:03:05 by npineau          ###   ########.fr       */
+/*   Created: 2014/01/27 14:03:27 by npineau           #+#    #+#             */
+/*   Updated: 2014/01/29 16:27:22 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "op.h"
 
-int	main(int argc, char **argv)
+int		**init_reg(void)
 {
-	t_opt	*options;
-	t_vm	*vm;
-	if (argc == 1)
-	{/* might need to create usage function */
-		write(1, "usage: ./corewar [-dump nbr_cycles] ", 36);
-		write(1, "[[-n number] champion1.cor] ...\n", 32);
-	}
-	else
-	{
-		options = gather_options(argc, argv);
-		vm = init_vm(options);
-		run_vm(vm, options);
-	}
-	return (0);
-}
+	int	*reg;
